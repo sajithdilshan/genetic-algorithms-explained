14622d95708fc05df4ab10248038837fcba80566
Sajith Dilshan <sajithdilshan@gmail.com>
2013-12-15 12:45:07 +0530

    added license info

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,8 @@
+//    Copyright © 2013 Sajith Dilshan <sajithdilshan@gmail.com>
+//    This work is free. You can redistribute it and/or modify it under the
+//    terms of the Do What The Fuck You Want To Public License, Version 2,
+//    as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+
 $(document).ready(function () {
 
     var positionList = [5, 2, 4, 7, 0, 3, 1, 6];
